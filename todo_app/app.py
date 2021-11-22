c21866c5c62af336eef98b8681596c6f62b06549
danny-flahive <93837720+danny-flahive@users.noreply.github.com>
2021-11-22 19:18:27 +0000

    Can Complete and Remove items, items are sorted
    Now that items are sorted, a new function is used to generate the ID when adding items to the list. Without this, the same ID was being given to multiple items, as the newest item wasn't automatically at the end of the list. New functions have also been added to remove items from the list and to Complete items.

--- todo_app/app.py
+++ todo_app/app.py
@@ -9,7 +9,6 @@ from todo_app.flask_config import Config
 app = Flask(__name__)
 app.config.from_object(Config())
 
-
 @app.route('/')
 def index():
     items = get_items()
@@ -18,4 +17,16 @@ def index():
 @app.route('/add', methods=['POST'])
 def add():
     add_item(request.form.get('title'))
+    return redirect('/')
+
+@app.route('/update/<int:id>')
+def mark_completed(id):
+    item = get_item(id)
+    item['status'] = "Completed"
+    save_item(item)
+    return redirect('/')
+
+@app.route('/remove/<int:id>')
+def remove(id):
+    remove_item(id)
     return redirect('/')
