f06a1808bff804d10e81a30d7665dcd1c42db668
danny-flahive <93837720+danny-flahive@users.noreply.github.com>
2021-11-22 18:09:40 +0000

    Shows todo list & allows adding items

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,4 +1,8 @@
 from flask import Flask
+from flask import render_template
+from flask import request
+from flask import redirect
+from todo_app.data.session_items import *
 
 from todo_app.flask_config import Config
 
@@ -8,4 +12,10 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    items = get_items()
+    return render_template('index.html', items=items)
+
+@app.route('/add', methods=['POST'])
+def add():
+    add_item(request.form.get('title'))
+    return redirect('/')
