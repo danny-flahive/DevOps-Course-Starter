658e7805bf01b66bc0358d5c6e36d3b22fa435a6
danny-flahive <93837720+danny-flahive@users.noreply.github.com>
2021-12-06 14:04:20 +0000

    Updated README, Added trello_items
    Lots of misc updates from adding new packages to the project.
    README has been updated to include new setup instructions.
    Trello_items now handles getting, adding, and changing status of items. Removal hasn't been implemented yet.
    This commit completes up to part 3 of this module exercise.

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,6 @@
-from flask import Flask
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect, Flask
 import todo_app.data.session_items as session_items
+import todo_app.data.trello_items as trello_items
 
 from todo_app.flask_config import Config
 
@@ -9,19 +9,20 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    items = session_items.get_items()
+    items = trello_items.get_items()
     return render_template('index.html', items=items)
 
 @app.route('/add', methods=['POST'])
 def add():
-    session_items.add_item(request.form.get('title'))
+    trello_items.add_item(request.form.get('title'))
     return redirect('/')
 
-@app.route('/update/<int:id>')
+@app.route('/update/<id>')
 def mark_completed(id):
-    item = session_items.get_item(id)
-    item['status'] = "Completed"
-    session_items.save_item(item)
+    # item = session_items.get_item(id)
+    # item['status'] = "Completed"
+    # session_items.save_item(item)
+    trello_items.complete_item(id)
     return redirect('/')
 
 @app.route('/remove/<int:id>')
---
+++ todo_app/data/trello_items.py
@@ -0,0 +1,35 @@
+import os, requests
+
+key = os.getenv("TRELLO_API_KEY")
+token = os.getenv("TRELLO_API_TOKEN")
+board = os.getenv("TRELLO_BOARD_ID")
+headers = {"Accept" : "application/json"}
+
+def get_items():
+    global cards
+    url = f"https://api.trello.com/1/boards/{board}/lists?key={key}&token={token}&cards=open"
+    response = requests.request("GET", url, headers=headers).json()
+    cards = [{}]
+    cards.clear()
+    for list in response:
+        for card in list["cards"]:
+            cards.append({"id" : card["id"], "title" : card["name"], "status" : list["name"]})
+    return cards
+
+def add_item(title: str):
+    list_id = __get_list_id__("To Do")
+    url = f"https://api.trello.com/1/cards?key={key}&token={token}&name={title}&idList={list_id}"
+    print(requests.request("POST", url, headers=headers))
+
+def complete_item(id: str):
+    new_list_id = __get_list_id__("Done")
+    url = f"https://api.trello.com/1/cards/{id}?key={key}&token={token}&idList={new_list_id}"
+    requests.request("PUT", url, headers=headers)
+
+def __get_list_id__(name: str) -> str:
+    url = f"https://api.trello.com/1/boards/{board}/lists?key={key}&token={token}"
+    response = requests.request("GET", url, headers=headers).json()
+    for list in response:
+        if list["name"] == name:
+            return list["id"]
+    return None
