5fa1b486ecab4c9a5bb3188b1e98f9770815ecf4
danny-flahive <93837720+danny-flahive@users.noreply.github.com>
2021-12-14 12:40:09 +0000

    Changed to using POST requests
    Updated the HTML to use forms and changed the routes to only accept post

--- todo_app/app.py
+++ todo_app/app.py
@@ -16,12 +16,12 @@ def add():
     trello_items.add_item(request.form.get('title'), request.form.get("description"))
     return redirect('/')
 
-@app.route('/update/<id>')
+@app.route('/update/<id>', methods=["POST"])
 def mark_completed(id: str):
     trello_items.complete_item(id)
     return redirect('/')
 
-@app.route('/remove/<id>')
+@app.route('/remove/<id>', methods=["POST"])
 def remove(id: str):
     trello_items.remove_item(id)
     return redirect('/')
