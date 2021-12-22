f231dbbd9ca12e431eebe2bed57d2adf49176bb2
danny-flahive <93837720+danny-flahive@users.noreply.github.com>
2021-12-22 14:11:47 +0000

    Update test_viewmodel.py
    I did a lot of stuff totally the wrong way before, now I've read more on testing wit pytest I think I've done this properly now. The tests were also just straight up wrong so that's been dealt with now. All tests pass

--- todo_app/tests/test_viewmodel.py
+++ todo_app/tests/test_viewmodel.py
@@ -1,45 +1,50 @@
-import pytest
+import pytest, requests
+from todo_app.Item import Item
 from todo_app.ViewModel import ViewModel
 
-from todo_app.data.trello_items import get_items, remove_item, add_item
+from todo_app.data.trello_items import get_items
 
 @pytest.fixture
-def setup_lists():
-    #Clear the board but save the original state
-    original_items = get_items()
-    for item in original_items:
-        remove_item(item.id)
-    #Add items
-    add_item("Done Test", board="Done")
-    add_item("Doing Test", board= "Doing")
-    add_item("To Do Test", board="To Do")
-    #Return items
-    test_items = get_items()
-    yield ViewModel(test_items)
-    #Teardown: restore board to it's original state
-    for item in test_items:
-        remove_item(item.id)
-    for item in original_items:
-        add_item(item.name, item.description, item.status)
+def viewmodel_test():
+    items = [Item("1", "To Do Test", status="To Do"), Item("2", "Doing Test", status="Doing"), Item("3", "Done Test", status="Done")]
+    return ViewModel(items)
 
-def test_pytestworks():
+def test_pytest_works():
     assert(True)
 
-def test_viewmodel_doing(setup_lists):
-    assert len(setup_lists.doing) == 1
-    assert setup_lists.doing[0].name == "Doing Test"
+def test_viewmodel_doing(viewmodel_test):
+    assert len(viewmodel_test.doing) == 1
+    assert viewmodel_test.doing[0].name == "Doing Test"
     with pytest.raises(IndexError):
-        setup_lists.doing[1]
+        viewmodel_test.doing[1]
 
 
-def test_viewmodel_todo(setup_lists):
-    assert len(ViewModel.done) == 1
-    assert ViewModel.done[0].name == "Done Test"
+def test_viewmodel_done(viewmodel_test):
+    assert len(viewmodel_test.done) == 1
+    assert viewmodel_test.done[0].name == "Done Test"
     with pytest.raises(IndexError):
-        setup_lists.done[1]
+        viewmodel_test.done[1]
 
-def test_viewmodel_done(setup_lists):
-    assert len(ViewModel.to_do) == 1
-    assert ViewModel.doing[0].name == "To Do Test"
+def test_viewmodel_todo(viewmodel_test):
+    assert len(viewmodel_test.to_do) == 1
+    assert viewmodel_test.to_do[0].name == "To Do Test"
     with pytest.raises(IndexError):
-        setup_lists.to_do[1]
+        viewmodel_test.to_do[1]
+    
+def test_get_items(monkeypatch):
+    monkeypatch.setattr(requests, "request", stub)
+    items = get_items()
+    assert len(items) == 4
+    assert items[0].name == "Test 1A"
+    assert items[1].name == "Test 2A"
+    assert items[2].name == "Test 1B"
+    assert items[3].name == "Test 2B"
+
+
+def stub(method, url, params, headers):
+    return StubResponse()
+
+class StubResponse:
+    def json(x):
+        response = [{"id" : "A", "name" : "Test A", "cards" : [{"id" : "1", "name" : "Test 1A", "desc" : ""}, {"id" : "2", "name" : "Test 2A", "desc" : ""}]}, {"id" : "B", "name" : "Test B", "cards" : [{"id" : "1", "name" : "Test 1B", "desc" : ""}, {"id" : "2", "name" : "Test 2B", "desc" : ""}]}]
+        return response
